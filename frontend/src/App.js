f0fe226a7ae7636649f3f23a208f03a4059419a5
Karan <packirisamykaran@gmail.com>
2023-03-18 11:26:00 +0800

    set

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,54 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
-    useEffect(() => {
-        let result;
+    const [query, setquery] = useState("");
+    const [suggestions, setSuggestions] = useState([]);
 
-        (async function () {
-            result = await axios.post(
-                process.env.REACT_APP_BACKEND_ENDPOINT + "getSuggestion",
+    async function getSuggestions(e) {
+        setquery(e.target.value);
+        let result = await axios.post(
+            process.env.REACT_APP_BACKEND_ENDPOINT + "getSuggestion",
 
-                { result: "tesst" }
-            );
+            { query: e.target.value }
+        );
 
-            console.log(result);
-        })();
-    });
+        console.log(result.data);
+        if (result.data.suggestions) {
+            setSuggestions(result.data.suggestions);
+        } else {
+            setSuggestions([]);
+        }
+    }
 
-    return <div className="App">{process.env.REACT_APP_BACKEND_ENDPOINT}</div>;
+    async function selectSuggestion(e) {
+        setSuggestions([]);
+        setquery(document.getElementById(e.target.id).getAttribute("value"));
+    }
+
+    return (
+        <div className="App">
+            <input
+                type="text"
+                onChange={(e) => getSuggestions(e)}
+                value={query}
+            />
+            {suggestions.map((i, sugg) => {
+                return (
+                    <div
+                        id={"sugg" + i}
+                        value={i}
+                        onClick={(e) => selectSuggestion(e)}>
+                        {i}
+                    </div>
+                );
+            })}
+
+            <div>result</div>
+        </div>
+    );
 }
 
 export default App;
--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
@@ -35,12 +39,16 @@ func main() {
 }
 
 type Data struct {
-	Result string `json:"result"`
+	Query string `json:"query"`
+}
+
+type ReturnData struct {
+	Suggestions []string `json:"suggestions"`
 }
 
 func Router() *mux.Router {
 	r := mux.NewRouter()
-	r.HandleFunc("/getSuggestion", getSuggestions).Methods("POST", "OPTIONS")
+	r.HandleFunc("/getSuggestion", getChatGPTresponse).Methods("POST", "OPTIONS")
 
 	return r
 }
@@ -49,53 +57,88 @@ func getSuggestions(w http.ResponseWriter, r *http.Request) {
 
 	enableCors(&w)
 
-	var body Data
+	var reqbody Data
 
-	_ = json.NewDecoder(r.Body).Decode(&body)
+	_ = json.NewDecoder(r.Body).Decode(&reqbody)
 
-	fmt.Printf("%+v", body.Result)
-	// query := string(querydata)
-	// querySplit := strings.Split(query, " ")
-	// query = strings.Join(querySplit, "%20")
+	query := string(reqbody.Query)
+	querySplit := strings.Split(query, " ")
+	query = strings.Join(querySplit, "%20")
+	// fmt.Printf("%+v \n", query)
+	url := "http://suggestqueries.google.com/complete/search?client=firefox&q=" + query
 
-	data := Data{
-		Result: "hello",
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer resp.Body.Close()
+	//We Read the response body on the line below.
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var arr []interface{}
+
+	err = json.Unmarshal(body, &arr)
+
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+
+	var sug []string
+
+	for _, v := range arr[1].([]interface{}) { // use type assertion to loop over []interface{}
+
+		val := v.(string)
+		sug = append(sug, val)
+	}
+
+	// fmt.Println(sug)
+
+	data := ReturnData{
+		Suggestions: sug,
 	}
 
 	json.NewEncoder(w).Encode(data)
 
-	// url := "http://suggestqueries.google.com/complete/search?client=firefox&q=" + query
+}
 
-	// resp, err := http.Get(url)
-	// if err != nil {
-	// 	log.Fatalln(err)
-	// }
+type ChatApiData struct {
+	Model    string `json:model`
+	Messages []MessageData
+}
 
-	// defer resp.Body.Close()
-	// //We Read the response body on the line below.
-	// body, err := io.ReadAll(resp.Body)
-	// if err != nil {
-	// 	log.Fatalln(err)
-	// }
+type MessageData struct {
+	Role    string `json:role`
+	Content string `json:content`
+}
 
-	// var arr []interface{}
+func getChatGPTresponse(w http.ResponseWriter, r *http.Request) {
+	url := "https://chatgpt-api.shn.hk/v1/"
+	enableCors(&w)
 
-	// err = json.Unmarshal(body, &arr)
+	var chatQeury ChatApiData = ChatApiData{
+		Model:    "gpt-3.5-turbo",
+		Messages: []MessageData{{Role: "user", Content: "who is thanos"}},
+	}
 
-	// if err != nil {
-	// 	fmt.Println("error:", err)
-	// }
+	chatQueryJson, _ := json.Marshal(chatQeury)
 
-	// var sug []string
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(chatQueryJson))
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-	// for _, v := range arr[1].([]interface{}) { // use type assertion to loop over []interface{}
+	defer resp.Body.Close()
+	//We Read the response body on the line below.
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-	// 	val := v.(string)
-	// 	sug = append(sug, val)
-	// }
-
-	// fmt.Println(sug)
-
+	fmt.Println(string(body))
 }
 
 func enableCors(w *http.ResponseWriter) {
